765c803d514c1edef4ee68e396867fcfdee22b4d
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-01-22 20:23:47 +0100

    all

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 function getElementWidth (content, padding, border){
-    const borderBox = content + padding + border;
-    return borderBox
+    content = Number.parseInt(content);
+    padding = Number.parseInt(padding);
+    border = Number.parseFloat(border);
+    return content + padding * 2 + border * 2;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px"));
